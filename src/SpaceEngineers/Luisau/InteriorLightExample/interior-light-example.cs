2e0f07f05e3cb231345041cfd3d79edfb9f9bfa5
Luis Augusto <luis@penapereira.com>
2020-07-22 18:58:25 -0300

    Add program template and light toggle example
    Details:
    * solutions file added
    * ms lib project file added
    * program template and interior light example added

---
+++ src/SpaceEngineers/Luisau/InteriorLightExample/interior-light-example.cs
@@ -0,0 +1,66 @@
+﻿#region Program Header
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+// Example program to toggle an interior light called "Interior Light"
+// every 200 ticks
+namespace SpaceEngineers.Luisau.InteriorLightExample
+{
+    public sealed class Program : MyGridProgram
+    {
+
+        #endregion
+        /////////////////////////////////////////////////////////////////
+        // Your code goes below this line
+
+        string lightName = "Interior Light";
+
+        int _count;
+        public Program()
+        {
+            if (Storage.Length > 0)
+            {
+                _count = int.Parse(Storage);
+            }
+            Runtime.UpdateFrequency = UpdateFrequency.Update100;
+            Echo("Program constructed");
+        }
+
+        public void Save()
+        {
+            Storage = _count + "";
+            Echo("Program saved");
+        }
+
+        public void Main(string args)
+        {
+            IMyInteriorLight light = (IMyInteriorLight)GridTerminalSystem.GetBlockWithName(lightName);
+            if (_count % 2 == 0)
+            {
+                Echo("Toggle light!");
+                light.ApplyAction("OnOff");
+                _count = 0;
+            }
+            Echo("executing, " + (_count++));
+        }
+
+        // Your code ends above this line
+        /////////////////////////////////////////////////////////////////
+        #region Program Footer
+    }
+}
+#endregion
---
+++ src/SpaceEngineers/Luisau/Template/program-template.cs
@@ -0,0 +1,67 @@
+﻿#region Program Header
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+// Change this namespace
+namespace SpaceEngineers.Luisau.Template {
+    public sealed class Program : MyGridProgram {
+
+#endregion
+/////////////////////////////////////////////////////////////////
+// Your code goes below this line
+
+public Program()
+{
+    // The constructor, called only once every session and
+    // always before any other method is called. Use it to
+    // initialize your script. 
+    //     
+    // The constructor is optional and can be removed if not
+    // needed.
+    // 
+    // It's recommended to set RuntimeInfo.UpdateFrequency 
+    // here, which will allow your script to run itself without a 
+    // timer block.
+}
+
+public void Save()
+{
+    // Called when the program needs to save its state. Use
+    // this method to save your state to the Storage field
+    // or some other means. 
+    // 
+    // This method is optional and can be removed if not
+    // needed.
+}
+
+public void Main(string argument, UpdateType updateSource)
+{
+    // The main entry point of the script, invoked every time
+    // one of the programmable block's Run actions are invoked,
+    // or the script updates itself. The updateSource argument
+    // describes where the update came from.
+    // 
+    // The method itself is required, but the arguments above
+    // can be removed if not needed.
+}
+
+// Your code ends above this line
+/////////////////////////////////////////////////////////////////
+#region Program Footer
+    }
+}
+#endregion
