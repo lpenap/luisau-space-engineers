5cfea99dfb81935a4f58180f6612550395c8a211
Luis Augusto Peña Pereira <lpenap@gmail.com>
2020-07-27 11:40:49 -0300

    Add program skell for drill program

---
+++ src/SpaceEngineers/Luisau/Drill/drill.cs
@@ -0,0 +1,41 @@
+﻿#region Prelude
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+using VRageMath;
+using VRage.Game;
+using VRage.Collections;
+using Sandbox.ModAPI.Ingame;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.Game.EntityComponents;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+
+// Change this namespace
+namespace SpaceEngineers.Luisau.Template {
+    public sealed class Program : MyGridProgram {
+
+#endregion
+/////////////////////////////////////////////////////////////////
+// Your code goes below this line
+
+public Program() {
+}
+
+public void Save() {
+}
+
+public void Main(string argument, UpdateType updateSource) {
+}
+
+// Your code ends above this line
+/////////////////////////////////////////////////////////////////
+#region PreludeFooter
+    }
+}
+#endregion
